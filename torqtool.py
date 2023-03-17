0ffbfce6c47ae322088d415b28c68acafa538b66
kthordarson <kthordarson@gmail.com>
2023-03-17 19:02:19 +0100

    cleanuptorqtool

--- torqtool.py
+++ torqtool.py
@@ -103,234 +103,6 @@ def sqlsender(buffer=None, dburl=None):
 	return results
 
 
-def read_process_ppe(dbtorqfiles, session):
-
-	read_res = []
-	tasklist = []
-	read_task_start = timer()
-	t0=datetime.now()
-	with ProcessPoolExecutor(max_workers=CPU_COUNT) as executor:
-		for tf in dbtorqfiles:
-			tasklist.append(executor.submit(read_buff, tf.csvfilefixed, tf.id, tf.tripid))
-	for res in as_completed(tasklist):
-		# set read_flag
-		r = res.result()
-		ntf = session.query(TorqFile).filter(TorqFile.id == r['fileid']).first()
-		ntf.read_flag = 1
-		#session.add(tf)
-		session.commit()
-		br = {
-			'fileid': r['fileid'],
-			'tripid': r['tripid'],
-			'torqbuffer': r['torqbuffer'],
-		}
-		read_res.append(br)
-	# buffer = [b for b in buffs]
-	read_task_end = timer()
-	logger.debug(f'read_res={len(read_res)} t={timedelta(seconds=read_task_end - read_task_start)} / {datetime.now()-t0}')
-	return read_res
-
-def read_process_tpe(dbtorqfiles, session):
-	read_res = []
-	tasklist = []
-	read_task_start = timer()
-	t0=datetime.now()
-	with ThreadPoolExecutor(max_workers=CPU_COUNT) as executor:
-		for tf in dbtorqfiles:
-			tasklist.append(executor.submit(read_buff, tf.csvfilefixed, tf.id, tf.tripid))
-	for res in as_completed(tasklist):
-		# set read_flag
-		r = res.result()
-		ntf = session.query(TorqFile).filter(TorqFile.id == r['fileid']).first()
-		ntf.read_flag = 1
-		#session.add(tf)
-		session.commit()
-		br = {
-			'fileid': r['fileid'],
-			'tripid': r['tripid'],
-			'torqbuffer': r['torqbuffer'],
-		}
-		read_res.append(br)
-	# buffer = [b for b in buffs]
-	read_task_end = timer()
-	logger.debug(f'read_res={len(read_res)} t={timedelta(seconds=read_task_end - read_task_start)} / {datetime.now()-t0}')
-	return read_res
-
-def send_process_ppe(buffs, session):
-	sendtasks = []
-	sendres = []
-	with ProcessPoolExecutor(max_workers=CPU_COUNT) as executor:
-		for idx, tchunk in enumerate(buffs):
-			sendtasks.append(executor.submit(sqlsender, tchunk, session))
-	for res in as_completed(sendtasks):
-		r = res.result()
-		if r:
-			sendres.append(r)
-	return sendres
-
-def send_process_tpe(buffs, session):
-	sendtasks = []
-	sendres = []
-	with ThreadPoolExecutor(max_workers=CPU_COUNT) as executor:
-		for idx, tchunk in enumerate(buffs):
-			sendtasks.append(executor.submit(sqlsender, tchunk, session))
-	for res in as_completed(sendtasks):
-		r = res.result()
-		if r:
-			sendres.append(r)
-	return sendres
-
-
-def send_data_process_ppe(dbtorqfiles, dburl):
-	sendtasks = []
-	sendres = []
-	with ProcessPoolExecutor(max_workers=CPU_COUNT) as executor:
-		for idx, tf in enumerate(dbtorqfiles):
-			sendtasks.append(executor.submit(send_torqdata,tf.id,dburl))
-	for res in as_completed(sendtasks):
-		try:
-			r = res.result()
-		except OperationalError as e:
-			logger.error(f'[sdp] OperationalError {e}')
-		if r:
-			sendres.append(r)
-	return sendres
-
-def send_data_process_tpe(dbtorqfiles, dburl):
-	sendtasks = []
-	sendres = []
-	with ThreadPoolExecutor(max_workers=CPU_COUNT) as executor:
-		for idx, tf in enumerate(dbtorqfiles):
-			sendtasks.append(executor.submit(send_torqdata,tf.id,dburl))
-	for res in as_completed(sendtasks):
-		try:
-			r = res.result()
-		except OperationalError as e:
-			logger.error(f'[sdp] OperationalError {e}')
-		if r:
-			sendres.append(r)
-	return sendres
-
-class TorqWorker(Thread):
-	def __init__(self, tf=None, dburl=None):
-		Thread.__init__(self)
-		self.tf = tf
-		self.dburl = dburl
-		#self.engine = create_engine(self.dburl, echo=False)
-		#self.Session = sessionmaker(bind=self.engine)
-		#self.Session = sessionmaker(bind=self.engine)
-		#self.session = self.Session()
-
-	def __repr__(self):
-		return f'Worker: {self.tf}'
-
-	def run(self):
-		buffer = read_buff(self.tf.csvfilefixed, self.tf.id, self.tf.tripid)
-		if buffer:
-			try:
-				results = sqlsender(buffer, self.dburl)
-			except TypeError as e:
-				errmsg = f'[TW] {self} typeerror {e}'
-				logger.error(errmsg)
-				return errmsg
-			datares = send_torqdata(self.tf.id, self.dburl)
-			return datares
-		else:
-			logger.error(f'[tw] no buffer from {self.tf.csvfilefixed}')
-			return None
-
-def get_torq_workers(torqfiles=None, dburl=None, engine=None, session=None):
-	workers = []
-	for tf in torqfiles:
-		w = TorqWorker(tf=tf, dburl=dburl, engine=None, session=session)
-		workers.append(w)
-	return workers
-
-def mainold(args):
-	# 1. scan args.path for csv files
-	# 2. check if csv files are in db
-	# 3. if not in db, foreach run fixer, create TorqFile and send to db
-	# 4.
-	# 5. read profile.properties from csvfile folder, foreach, create Torqtrips and send to db
-	# 6. foreach new TorqFile, read fixed csv, create TorqLogs and send to db
-	# 7. read_process_ppe collects csvdata, insert columns fileid and tripid
-	# 8. send csvdata to db
-	# todo: create worker thread for each file, worker reads and processes file and sends to db.
-	t0 = datetime.now()
-	dburl = None
-	if args.dbmode == 'mysql':
-		dburl = f"mysql+pymysql://{args.dbuser}:{args.dbpass}@{args.dbhost}/{args.dbname}?charset=utf8mb4"
-		engine = create_engine(dburl)
-		Session = sessionmaker(bind=engine)
-		session = Session()
-	elif args.dbmode == 'postgresql':
-		dburl = f"postgresql://{args.dbuser}:{args.dbpass}@{args.dbhost}/{args.dbname}"
-		engine = create_engine(dburl)
-		Session = sessionmaker(bind=engine)
-		session = Session()
-	elif args.dbmode == 'sqlite':
-		dburl = f'sqlite:///torqfiskurdb'
-		engine = create_engine(dburl, echo=False, connect_args={'check_same_thread': False})
-		Session = sessionmaker(bind=engine)
-		session = Session()
-		# session.execute(text('PRAGMA foreign_keys=OFF'))
-		sqlite_db_init(engine)
-	if args.init_db:
-		try:
-			database_init(session, engine)
-		except OperationalError as e:
-			logger.error(f'[database_init] {e}')
-
-	filelist = get_csv_files(searchpath=Path(args.path), dbmode=args.dbmode)
-	newfilelist = []
-	newfilelist = send_torqfiles(filelist, session)
-	if newfilelist is None:
-		logger.warning(f'[main]	send_torqfiles returned None')
-		sys.exit(1)
-	elif len(newfilelist) == 0:
-		logger.warning(f'[main]	0 files from send_torqfiles....')
-		sys.exit(1)
-	else:
-		# get files from db that are not read
-		tripstart = timer()
-		dbtorqfiles = session.query(TorqFile).filter(TorqFile.read_flag == 0).all()
-		for torqfile in dbtorqfiles:
-			send_torqtrips(torqfile, session)
-		tripend = timer()
-		logger.debug(f'[send_torqtrips] done t0={datetime.now()-t0} time={timedelta(seconds=tripend - tripstart)} starting read_process mode={args.threadmode}')
-		readstart = timer()
-		if args.threadmode == 'ppe':
-			buffs = read_process_ppe(dbtorqfiles, session)
-		elif args.threadmode == 'tpe':
-			buffs = read_process_tpe(dbtorqfiles, session)
-		readend = timer()
-		logger.debug(f'[read_process] done t0={datetime.now()-t0} time={timedelta(seconds=readend - readstart)} buffs:{len(buffs)} starting send_process mode={args.threadmode}')
-		send_start = timer()
-		if args.threadmode == 'ppe':
-			sendres = send_process_ppe(buffs, dburl)
-		elif args.threadmode == 'tpe':
-			sendres = send_process_tpe(buffs, dburl)
-		for r in sendres:
-			if r['status'] == 'success':
-				# sent send_flag on successful send
-				ntf = session.query(TorqFile).filter(TorqFile.id == r['fileid']).first()
-				ntf.send_flag = 1
-				session.commit()
-		send_end = timer()
-
-		datastart = timer()
-		logger.debug(f'[send_process] done t0={datetime.now()-t0} time={timedelta(seconds=send_end - send_start)} starting send_data_process mode={args.threadmode}')
-		#for tf in dbtorqfiles:
-		#	send_torqdata(tf, session, engine)
-		if args.threadmode == 'ppe':
-			send_data_process_ppe(dbtorqfiles, dburl)
-		elif args.threadmode == 'tpe':
-			send_data_process_tpe(dbtorqfiles, dburl)
-		# create_tripdata(engine, session, newfilelist)
-		dataend = timer()
-
-		logger.info(f'[*] timers t0={datetime.now()-t0} readtime={timedelta(seconds=readend - readstart)} sendtime={timedelta(seconds=send_end - send_start)} triptime={timedelta(seconds=tripend - tripstart)} datatime={timedelta(seconds=dataend - datastart)} buffs:{len(buffs)} dbtorqfiles={len(dbtorqfiles)} CPU_COUNT={CPU_COUNT}')
-		# fixtime={timedelta(seconds=fix_end - fix_start)} uptime={timedelta(seconds=up_end - up_start)}
 
 def torq_worker(tf, dburl):
 	try:
@@ -348,8 +120,6 @@ def torq_worker(tf, dburl):
 	}
 	return res
 
-
-
 def main(args):
 	# 1. scan args.path for csv files
 	# 2. check if csv files are in db
@@ -357,7 +127,7 @@ def main(args):
 	# 4.
 	# 5. read profile.properties from csvfile folder, foreach, create Torqtrips and send to db
 	# 6. foreach new TorqFile, read fixed csv, create TorqLogs and send to db
-	# 7. read_process_ppe collects csvdata, insert columns fileid and tripid
+	# 7.
 	# 8. send csvdata to db
 	# todo: create worker thread for each file, worker reads and processes file and sends to db.
 	t0 = datetime.now()
@@ -404,7 +174,6 @@ def main(args):
 			send_torqtrips(torqfile, session)
 		tripend = timer()
 		logger.debug(f'[send_torqtrips] done t0={datetime.now()-t0} time={timedelta(seconds=tripend - tripstart)} starting read_process for {len(dbtorqfiles)} files mode={args.threadmode}')
-		#workers = get_torq_workers(torqfiles=dbtorqfiles, dburl=dburl, engine=engine, session=session)
 		tasks = []
 		if args.threadmode == 'ppe':
 			with ProcessPoolExecutor(max_workers=CPU_COUNT) as executor:
