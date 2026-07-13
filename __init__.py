f9f50f2e90d3ca597d9f674dfddba838b8769b17
kthordarson <kthordarson@gmail.com>
2026-07-13 15:21:16 +0000

    move gui stuff

--- __init__.py
+++
@@ -1,8 +0,0 @@
-"""
-A thing for torquepro data.
-"""
-
-from . import torqtool, converter, datamodels, utils
-
-__version__ = '1.0.0'
-__all__ = ['torqtool','converter','schemas', 'datamodels', 'utils']
--- __main__.py
+++
@@ -1,4 +0,0 @@
-# entry point
-from .converter import main
-if __name__ == '__main__':
-	main()
