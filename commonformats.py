ef6bd5a8796cf9963f8a0fa5c83bb88a34fe7e4b
kthordarson <kthordarson@gmail.com>
2026-07-13 15:38:50 +0000

    commonformats.py

--- commonformats.py
+++ commonformats.py
@@ -1,19 +1,11 @@
-fmt_19 = '%d-%b-%Y %H:%M:%S'
-fmt_20 = '%d-%b-%Y %H:%M:%S'
+fmt_19 = "%d-%b-%Y %H:%M:%S"
+fmt_20 = "%d-%b-%Y %H:%M:%S"
+fmt_24 = "%d-%b-%Y %H:%M:%S.%f"
+fmt_26 = "%Y-%m-%d %H:%M:%S.%f"
+fmt_28 = "%a %b %d %H:%M:%S GMT %Y"
+fmt_30 = "%a %b %d %H:%M:%S %Z %Y"
+fmt_34 = "%a %b %d %H:%M:%S %Z%z %Y"
+fmt_36 = "%a %b %d %H:%M:%S %Z%z %Y"
 
-fmt_24 = '%d-%b-%Y %H:%M:%S.%f'
-# len('29-Jun-2022 18:12:15.661')
-
-fmt_26 = '%Y-%m-%d %H:%M:%S.%f'
-
-fmt_28 = '%a %b %d %H:%M:%S GMT %Y'
-
-fmt_30 = '%a %b %d %H:%M:%S %Z %Y'
-
-fmt_34 = '%a %b %d %H:%M:%S %Z%z %Y'
-# len('Wed Jun 29 18:12:15 GMT+02:00 2022')
-
-fmt_36 = '%a %b %d %H:%M:%S %Z%z %Y'
-
-if __name__ == '__main__':
-	pass
+if __name__ == "__main__":
+    pass
